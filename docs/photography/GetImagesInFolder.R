2348af6545cc54ebffbc396dcd8076444de9b845
Daniel Roelfs <daniel@roelfs.net>
2021-01-13 22:16:35 +0100

    update build

---
+++ docs/photography/GetImagesInFolder.R
@@ -0,0 +1,9 @@
+### CREATE HTML FOR IMAGES ########################
+
+loi <- list.files(here::here("content/photography/img/"))
+
+html_template <- '\t\t\t\t\t<img src="./img/%s">'
+
+html_text <- sprintf(html_template, loi)                  
+
+readr::write_lines(html_text, file = sprintf("%s/img_files.txt",here::here("content/photography/")))
